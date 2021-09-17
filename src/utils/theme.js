14c3c33031f3295393b047580094314997a087d4
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-09-17 21:30:17 +0300

    add setup

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+export const theme = Object.freeze({
+  colors: {
+    white: "#ffffff",
+    BorderColor: "rgb(255, 222, 173)",
+    InputColor: "rgba(255, 248, 220, 0.5)",
+    ButtonColor: "rgb(255, 248, 220)",
+  },
+  animation: "250ms",
+  time: "cubic-bezier(0.4, 0, 0.2, 1)",
+});

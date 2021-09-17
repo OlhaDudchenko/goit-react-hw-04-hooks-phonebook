13ad81c125e0ed8742aa199328a9f47b01e3b5f7
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-09-17 21:37:12 +0300

    change code on hooks

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Section } from "./components/Section";
+import { ContactForm } from "./components/ContactForm";
+import { ContactList } from "./components/ContactList";
+import { Filter } from "./components/Filter";
+import { contactsList } from "./services/contacts";
+
+export function App() {
+  const [contacts, setContacts] = useState(contactsList);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const contacts = localStorage.getItem("contacts");
+
+    const parsedContacts = JSON.parse(contacts);
+
+    setContacts(parsedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    const filteredContact = contacts.find(
+      (contact) => contact.name === newContact.name
+    );
+
+    filteredContact
+      ? alert(`${newContact.name}is already in contacts`)
+      : setContacts([newContact, ...contacts]);
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((contacts) =>
+      contacts.filter((contact) => contact.id !== contactId)
+    );
+  };
+
+  const changeFilter = (event) => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const getVisibleContact = () => {
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+  return (
+    <div>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter value={filter} onChange={changeFilter} />
+
+        <ContactList
+          contacts={getVisibleContact()}
+          onDeleteContact={deleteContact}
+        />
+      </Section>
+    </div>
+  );
+}
